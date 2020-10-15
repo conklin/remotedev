5b28f59ede9c2066ea2cd1d303de6367189f4ef6
mconklin <ssi.conklin@gmail.com>
2020-10-14 20:03:54 -0400

    initial vm temp def

--- main.tf
+++ main.tf
@@ -2,3 +2,46 @@ provider "google" {
   project     = var.project_id
   region      = var.compute_region
 }
+
+resource "google_compute_instance_template" "dev_env_server_template" {
+  name        = "dev-server-template"
+
+  labels = {
+    environment = "dev"
+  }
+
+  machine_type         = "n1-standard-1"
+  can_ip_forward       = false
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+  }
+
+  disk {
+    source      = google_compute_disk.dev_disk.name
+    auto_delete = false
+    boot        = false
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+#   service_account {
+#     scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+#   }
+}
+
+resource "google_compute_disk" "dev_disk" {
+  name  = "existing-disk"
+  image = data.google_compute_image.dev_image.self_link
+  size  = 10
+  type  = "pd-ssd"
+  zone  = "us-central1-a"
+}
+
+data "google_compute_image" "dev_image" {
+  family  = "debian-9"
+  project = "debian-cloud"
+}
