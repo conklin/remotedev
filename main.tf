623635fd148635f3d478e16d1389997446a98eff
mconklin <ssi.conklin@gmail.com>
2020-10-15 22:38:27 -0400

    define router and nat

--- main.tf
+++ main.tf
@@ -59,3 +59,21 @@ resource "google_compute_subnetwork" "dev-env-sub-network" {
   ip_cidr_range = "10.0.0.0/22"
   network = google_compute_network.dev-env-network.id
 }
+
+resource "google_compute_router" "router" {
+  name    = "my-router"
+  network = google_compute_network.dev-env-network.name
+}
+
+resource "google_compute_router_nat" "nat" {
+  name                               = "my-router-nat"
+  router                             = google_compute_router.router.name
+  region                             = google_compute_router.router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
