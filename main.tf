1b3f163c1576ad3097a6e58c9e44e4aaa44c75e2
mconklin <ssi.conklin@gmail.com>
2020-10-15 21:42:47 -0400

    define network config

--- main.tf
+++ main.tf
@@ -35,13 +35,27 @@ resource "google_compute_instance_template" "dev_env_server_template" {
 
 resource "google_compute_disk" "dev_disk" {
   name  = "dev-server-disk"
-  image = data.google_compute_image.dev_image.self_link
+  image = google_compute_image.dev_image.id
   size  = 10
   type  = "pd-ssd"
   zone  = var.compute_zone
 }
 
-data "google_compute_image" "dev_image" {
+resource "google_compute_image" "dev_image" {
+  name = "base-os"  
   family  = "debian-9"
   project = "debian-cloud"
+}
+
+
+resource "google_compute_network" "dev-env-network" {
+  name = "default-us-east1"
+}
+
+resource "google_compute_subnetwork" "dev-env-sub-network" {
+  name = "dev-subnet"
+  region = var.compute_region
+  private_ip_google_access = true
+  ip_cidr_range = "10.0.0.0/22"
+  network = google_compute_network.dev-env-network.id
 }
